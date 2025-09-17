79a0914016d921a0c25be59f343effd03b606bf4
Patience Bwire <69461416+Patience-B@users.noreply.github.com>
2025-09-17 16:16:08 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -15,7 +15,22 @@ const data = [
     { date: "30/08/2025", week: "25/08/2025", bible: 0, steps: 2000, sand: 0, portfolio: 0, screenTime: "5:31:00", accomplishment: "Went to visit the Jerry. " },
     { date: "31/08/2025", week: "25/08/2025", bible: 42, steps: 6500, sand: 1, portfolio: 0, screenTime: "4:58:00", accomplishment: "" },
     { date: "01/09/2025", week: "01/09/2025", bible: 15, steps: 7500, sand: 2, portfolio: 2, screenTime: "2:59:00", accomplishment: "Did a presentation for Precise conference" },
-    { date: "02/09/2025", week: "01/09/2025", bible: 66, steps: 0, sand: 1.5, portfolio: 0, screenTime: "10:59:00", accomplishment: "" }
+    { date: "02/09/2025", week: "01/09/2025", bible: 66, steps: 0, sand: 1.5, portfolio: 0, screenTime: "10:59:00", accomplishment: "" },
+    { date: "03/09/2025", week: "01/09/2025", bible: 7, steps: 12000, sand: 3, portfolio: 0, screenTime: "3:41:00", accomplishment: "Went to ABSA, Stanbic, Airtel service center, MTN service center and Bible study" },
+    { date: "04/09/2025", week: "01/09/2025", bible: 5, steps: 0, sand: 2, portfolio: 0, screenTime: "7:30:00", accomplishment: "Bible study with Tim, ate healthy" },
+    { date: "05/09/2025", week: "01/09/2025", bible: 0, steps: 7000, sand: 2, portfolio: 0, screenTime: "6:15:00", accomplishment: "Did a long evening walk" },
+    { date: "06/09/2025", week: "01/09/2025", bible: 0, steps: 8400, sand: 0, portfolio: 0, screenTime: "4:29:00", accomplishment: "Went for Homes expo plus Giki's prayer thing" },
+    { date: "07/09/2025", week: "01/09/2025", bible: 0, steps: 8000, sand: 0, portfolio: 0, screenTime: "8:35:00", accomplishment: "I have been eating relatively cleanly for the past few days. I went to Sentama to see Mirembe Estates and was convinced" },
+    { date: "08/09/2025", week: "08/09/2025", bible: 0, steps: 8000, sand: 3, portfolio: 0, screenTime: "7:11:00", accomplishment: "Went to Lugogo to fix my MTN simcard issues" },
+    { date: "09/09/2025", week: "08/09/2025", bible: 0, steps: 0, sand: 3, portfolio: 0, screenTime: "7:47:00", accomplishment: "" },
+    { date: "10/09/2025", week: "08/09/2025", bible: 0, steps: 8000, sand: 3, portfolio: 0, screenTime: "8:51:00", accomplishment: "Went to Mirembe estates Sentama to get my house" },
+    { date: "11/09/2025", week: "08/09/2025", bible: 0, steps: 6800, sand: 3, portfolio: 0, screenTime: "8:17:00", accomplishment: "Went to see Uncle Danny" },
+    { date: "12/09/2025", week: "08/09/2025", bible: 0, steps: 0, sand: 2, portfolio: 0, screenTime: "7:59:00", accomplishment: "" },
+    { date: "13/09/2025", week: "08/09/2025", bible: 0, steps: 3000, sand: 0, portfolio: 0, screenTime: "6:29:00", accomplishment: "Washed dishes, did some housework, cooked a healthy meal for the girls" },
+    { date: "14/09/2025", week: "08/09/2025", bible: 0, steps: 2000, sand: 0, portfolio: 0, screenTime: "12:21:00", accomplishment: "Went to church, cleaned my room" },
+    { date: "15/09/2025", week: "15/09/2025", bible: 0, steps: 0, sand: 3, portfolio: 0, screenTime: "7:35:00", accomplishment: "Went to clinic for test" },
+    { date: "16/09/2025", week: "15/09/2025", bible: 12, steps: 3000, sand: 3, portfolio: 0, screenTime: "10:28:00", accomplishment: "Did prayer card" }
+
 ];
 
 // Goals configuration
